892e2c1642353130dc264c435dc5bbb381b80455
Django-strange <devarajk0323@gmail.com>
2025-06-28 15:05:40 +0530

    Initial commit with JWT authentication

--- seedapp_microservices/product_service/product_project/settings.py
+++ seedapp_microservices/product_service/product_project/settings.py
@@ -47,8 +47,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'rest_framework', 
-    'corsheaders',  
+    'corsheaders',
+    'rest_framework_simplejwt',
+
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 
 CORS_ALLOWED_ORIGINS = [
